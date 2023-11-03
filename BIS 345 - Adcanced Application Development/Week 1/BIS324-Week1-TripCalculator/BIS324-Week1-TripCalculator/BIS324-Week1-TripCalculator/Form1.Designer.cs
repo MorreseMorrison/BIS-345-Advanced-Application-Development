927b58d17423f16527c1d8f764afa11a1b7bf126
MorreseMorrison <rese298@gmail.com>
2023-11-03 13:39:06 -0400

    Add files via upload
    FINAL CODE COMMIT COMPLETE

---
+++ BIS 345 - Adcanced Application Development/Week 1/BIS324-Week1-TripCalculator/BIS324-Week1-TripCalculator/BIS324-Week1-TripCalculator/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace BIS324_Week1_TripCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1StarRentalsHeader = new System.Windows.Forms.TextBox();
+            this.Label1EnterDestinationDescription = new System.Windows.Forms.Label();
+            this.Label2EnterMilesTraveledDestination = new System.Windows.Forms.Label();
+            this.TextBox2CarTypeGallonsDescription = new System.Windows.Forms.TextBox();
+            this.TextBox3EnterDestination = new System.Windows.Forms.TextBox();
+            this.TextBox4EnterMiles = new System.Windows.Forms.TextBox();
+            this.Button1Calculate = new System.Windows.Forms.Button();
+            this.Button2Clear = new System.Windows.Forms.Button();
+            this.Label3MpgOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox1StarRentalsHeader
+            // 
+            this.TextBox1StarRentalsHeader.BackColor = System.Drawing.SystemColors.Info;
+            this.TextBox1StarRentalsHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1StarRentalsHeader.Location = new System.Drawing.Point(28, 50);
+            this.TextBox1StarRentalsHeader.Name = "TextBox1StarRentalsHeader";
+            this.TextBox1StarRentalsHeader.Size = new System.Drawing.Size(1182, 49);
+            this.TextBox1StarRentalsHeader.TabIndex = 0;
+            this.TextBox1StarRentalsHeader.Text = "Welcome To Star Rentals Trip Destination Calculator!";
+            this.TextBox1StarRentalsHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TextBox1StarRentalsHeader.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Label1EnterDestinationDescription
+            // 
+            this.Label1EnterDestinationDescription.AutoSize = true;
+            this.Label1EnterDestinationDescription.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Label1EnterDestinationDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1EnterDestinationDescription.Location = new System.Drawing.Point(188, 235);
+            this.Label1EnterDestinationDescription.Name = "Label1EnterDestinationDescription";
+            this.Label1EnterDestinationDescription.Size = new System.Drawing.Size(483, 37);
+            this.Label1EnterDestinationDescription.TabIndex = 1;
+            this.Label1EnterDestinationDescription.Text = "Please Type In Your Destination:";
+            this.Label1EnterDestinationDescription.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Label2EnterMilesTraveledDestination
+            // 
+            this.Label2EnterMilesTraveledDestination.AllowDrop = true;
+            this.Label2EnterMilesTraveledDestination.AutoSize = true;
+            this.Label2EnterMilesTraveledDestination.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Label2EnterMilesTraveledDestination.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2EnterMilesTraveledDestination.Location = new System.Drawing.Point(188, 336);
+            this.Label2EnterMilesTraveledDestination.Name = "Label2EnterMilesTraveledDestination";
+            this.Label2EnterMilesTraveledDestination.Size = new System.Drawing.Size(421, 37);
+            this.Label2EnterMilesTraveledDestination.TabIndex = 2;
+            this.Label2EnterMilesTraveledDestination.Text = "Please Enter Miles Traveled:";
+            this.Label2EnterMilesTraveledDestination.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TextBox2CarTypeGallonsDescription
+            // 
+            this.TextBox2CarTypeGallonsDescription.AllowDrop = true;
+            this.TextBox2CarTypeGallonsDescription.BackColor = System.Drawing.SystemColors.Info;
+            this.TextBox2CarTypeGallonsDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox2CarTypeGallonsDescription.Location = new System.Drawing.Point(193, 124);
+            this.TextBox2CarTypeGallonsDescription.Multiline = true;
+            this.TextBox2CarTypeGallonsDescription.Name = "TextBox2CarTypeGallonsDescription";
+            this.TextBox2CarTypeGallonsDescription.Size = new System.Drawing.Size(916, 41);
+            this.TextBox2CarTypeGallonsDescription.TabIndex = 3;
+            this.TextBox2CarTypeGallonsDescription.Text = "You Rented A 2023 Honda Accord Which Holds 14.8 Gallons";
+            this.TextBox2CarTypeGallonsDescription.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TextBox2CarTypeGallonsDescription.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // TextBox3EnterDestination
+            // 
+            this.TextBox3EnterDestination.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TextBox3EnterDestination.Location = new System.Drawing.Point(813, 241);
+            this.TextBox3EnterDestination.Multiline = true;
+            this.TextBox3EnterDestination.Name = "TextBox3EnterDestination";
+            this.TextBox3EnterDestination.Size = new System.Drawing.Size(156, 45);
+            this.TextBox3EnterDestination.TabIndex = 4;
+            this.TextBox3EnterDestination.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // TextBox4EnterMiles
+            // 
+            this.TextBox4EnterMiles.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TextBox4EnterMiles.Location = new System.Drawing.Point(813, 336);
+            this.TextBox4EnterMiles.Multiline = true;
+            this.TextBox4EnterMiles.Name = "TextBox4EnterMiles";
+            this.TextBox4EnterMiles.Size = new System.Drawing.Size(156, 47);
+            this.TextBox4EnterMiles.TabIndex = 5;
+            this.TextBox4EnterMiles.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.TextBox4EnterMiles.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Button1Calculate
+            // 
+            this.Button1Calculate.Location = new System.Drawing.Point(813, 426);
+            this.Button1Calculate.Name = "Button1Calculate";
+            this.Button1Calculate.Size = new System.Drawing.Size(188, 78);
+            this.Button1Calculate.TabIndex = 6;
+            this.Button1Calculate.Text = "CALCULATE";
+            this.Button1Calculate.UseVisualStyleBackColor = true;
+            this.Button1Calculate.Click += new System.EventHandler(this.Button1Calculate_Click);
+            // 
+            // Button2Clear
+            // 
+            this.Button2Clear.Location = new System.Drawing.Point(195, 426);
+            this.Button2Clear.Name = "Button2Clear";
+            this.Button2Clear.Size = new System.Drawing.Size(200, 78);
+            this.Button2Clear.TabIndex = 7;
+            this.Button2Clear.Text = "CLEAR";
+            this.Button2Clear.UseVisualStyleBackColor = true;
+            this.Button2Clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Label3MpgOutput
+            // 
+            this.Label3MpgOutput.AutoSize = true;
+            this.Label3MpgOutput.Location = new System.Drawing.Point(190, 572);
+            this.Label3MpgOutput.Name = "Label3MpgOutput";
+            this.Label3MpgOutput.Size = new System.Drawing.Size(0, 25);
+            this.Label3MpgOutput.TabIndex = 8;
+            this.Label3MpgOutput.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1222, 772);
+            this.Controls.Add(this.Label3MpgOutput);
+            this.Controls.Add(this.Button2Clear);
+            this.Controls.Add(this.Button1Calculate);
+            this.Controls.Add(this.TextBox4EnterMiles);
+            this.Controls.Add(this.TextBox3EnterDestination);
+            this.Controls.Add(this.TextBox2CarTypeGallonsDescription);
+            this.Controls.Add(this.Label2EnterMilesTraveledDestination);
+            this.Controls.Add(this.Label1EnterDestinationDescription);
+            this.Controls.Add(this.TextBox1StarRentalsHeader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1StarRentalsHeader;
+        private System.Windows.Forms.Label Label1EnterDestinationDescription;
+        private System.Windows.Forms.Label Label2EnterMilesTraveledDestination;
+        private System.Windows.Forms.TextBox TextBox2CarTypeGallonsDescription;
+        private System.Windows.Forms.TextBox TextBox3EnterDestination;
+        private System.Windows.Forms.TextBox TextBox4EnterMiles;
+        private System.Windows.Forms.Button Button1Calculate;
+        private System.Windows.Forms.Button Button2Clear;
+        private System.Windows.Forms.Label Label3MpgOutput;
+    }
+}
+
---
+++ BIS 345 - Adcanced Application Development/Week 1/BIS324-Week1-TripCalculator/BIS324-Week1-TripCalculator/BIS324-Week1-TripCalculator/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIS324_Week1_TripCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Attention: This Label Is Hidden At The Bottom Of The Form On The Left Hand Side Directl Below The Clear Button
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //FORM BUTTON = **CLEAR BUTTON**//
+            //*EXTRA* Logic To Clear Both Text Boxes. During Run Time The User May
+            //Enter In Text Within The And Can Clear If Need Be Once They Click
+            //On The Button.
+     
+            TextBox3EnterDestination.Text = String.Empty;
+            TextBox4EnterMiles.Text = String.Empty;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1Calculate_Click(object sender, EventArgs e)
+        {
+            //FORM BUTTON = **CALCULATE**//
+
+
+
+            //**MAIN LOGIC**//
+
+            //VARIABLES 
+            string DestinationTraveled; //This Variable Will Contain An Input From The User When Asked Their Destination
+            double HondaGallonCapacity = 14.8; //This Varible Is Set To 14.8 As This Is The Tank Capacity For The 2023 Honda Accord
+            double HondaMaximumMileageOneFullTank = 653;
+            double MilesTraveled; //This Variable Will Contain An Input From The User When Asked Of Their Miles Traveled
+            double MilesPerGallon; //This Varable Will Contain The End Value For The MPG Logic Calculation In Heading (3)
+             
+
+
+            //**PROGRAM START**//
+
+            //(Code Block-1) Text From The "Please Type In Your Destination:" Question Is Entered
+            //And Stored In "String Variable DestinationTraveled"
+            DestinationTraveled = TextBox3EnterDestination.Text;
+
+
+            //(Code Block-2)Text From The "Please Enter Miles Traveled: Question Is Entered And Stored In "Double Variable MilesTraveled"
+            //**Also, We Are Utilizing The Parse Converter To Convert The String Into Type Double So That It May Be Used In Our MPG Logic Calculation**
+
+            MilesTraveled = double.Parse(TextBox4EnterMiles.Text);
+
+            //(Code Block-2.1)If Logic In The Event A User Enters A Value Greater Then The Maximum Mileage Of One Full Tank On A Honda Accord.
+            if (MilesTraveled > HondaMaximumMileageOneFullTank) {
+
+                //Message Box Will Appear With An "OK" Button
+                MessageBox.Show("THE MAXIMUM MILES THAT CAN BE TRAVELED ON ONE TANK OF GAS IS 653 Miles. Please Try Again", "PLEASE TRY AGAIN", MessageBoxButtons.OK);
+
+                //After Selecting Ok The Program Will Restart, Clearing All Fields
+                Application.Restart();
+            }
+
+
+
+            //(Code Block-4) Miles Per Gallon Logic:
+            //Pseudo Code: Total Miles Traveled Divided By The Car's Gallon Capacity = MPG
+            MilesPerGallon = MilesTraveled / HondaGallonCapacity;
+
+          
+
+            //(Code Block-4) Print Out Of The Destination: Traveled As Well As The MPG In Precision Format. This Is Being Fed To A Hidden Label At The Bottom Of The Form
+            Label3MpgOutput.Text = String.Format("You Traveled To " + DestinationTraveled + "! Your MPG For This Trip Was " + MilesPerGallon.ToString("0.00") + " Miles!" );
+
+            //**END**//
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+           
+
+          
+
+
+        }
+    }
+}
