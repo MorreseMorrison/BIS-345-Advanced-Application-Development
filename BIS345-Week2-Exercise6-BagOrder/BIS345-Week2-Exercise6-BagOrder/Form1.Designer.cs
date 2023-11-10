ceb0636b081313868f291e3e3985b75290714e62
MorreseMorrison <rese298@gmail.com>
2023-11-10 11:23:47 -0500

    Add files via upload

--- BIS345-Week2-Exercise6-BagOrder/BIS345-Week2-Exercise6-BagOrder/Form1.Designer.cs
+++ BIS345-Week2-Exercise6-BagOrder/BIS345-Week2-Exercise6-BagOrder/Form1.Designer.cs
@@ -32,12 +32,10 @@
             this.DescriptionLabel2 = new System.Windows.Forms.Label();
             this.EmployeeNameTextBoxLabel3 = new System.Windows.Forms.TextBox();
             this.FullNameLabel3 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.PleaseSelectTodayDateLabel4 = new System.Windows.Forms.Label();
             this.BagListBox1 = new System.Windows.Forms.ListBox();
             this.PleaseSelectYourBagLabel5 = new System.Windows.Forms.Label();
             this.PleaseSelectYourQuantityLabel6 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SelectQuantityComboBox1 = new System.Windows.Forms.ComboBox();
             this.ClearButton2 = new System.Windows.Forms.Button();
             this.OrderButton4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -48,12 +46,12 @@
             this.TitleLabel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.TitleLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitleLabel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.TitleLabel1.Location = new System.Drawing.Point(709, 43);
+            this.TitleLabel1.Location = new System.Drawing.Point(523, 43);
             this.TitleLabel1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.TitleLabel1.Name = "TitleLabel1";
-            this.TitleLabel1.Size = new System.Drawing.Size(1024, 55);
+            this.TitleLabel1.Size = new System.Drawing.Size(918, 55);
             this.TitleLabel1.TabIndex = 0;
-            this.TitleLabel1.Text = "Employee Internal Stock Ordering Form (Bags)";
+            this.TitleLabel1.Text = "PARTY STATE USA BAG ORDER FORM";
             this.TitleLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.TitleLabel1.Click += new System.EventHandler(this.TitleLabel1_Click);
             // 
@@ -61,7 +59,7 @@
             // 
             this.DescriptionLabel2.AutoSize = true;
             this.DescriptionLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DescriptionLabel2.Location = new System.Drawing.Point(1050, 127);
+            this.DescriptionLabel2.Location = new System.Drawing.Point(798, 110);
             this.DescriptionLabel2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.DescriptionLabel2.Name = "DescriptionLabel2";
             this.DescriptionLabel2.Size = new System.Drawing.Size(385, 31);
@@ -70,41 +68,24 @@
             // 
             // EmployeeNameTextBoxLabel3
             // 
-            this.EmployeeNameTextBoxLabel3.Location = new System.Drawing.Point(477, 302);
+            this.EmployeeNameTextBoxLabel3.Location = new System.Drawing.Point(975, 259);
             this.EmployeeNameTextBoxLabel3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.EmployeeNameTextBoxLabel3.MaxLength = 25;
             this.EmployeeNameTextBoxLabel3.Multiline = true;
             this.EmployeeNameTextBoxLabel3.Name = "EmployeeNameTextBoxLabel3";
-            this.EmployeeNameTextBoxLabel3.Size = new System.Drawing.Size(694, 44);
+            this.EmployeeNameTextBoxLabel3.Size = new System.Drawing.Size(572, 44);
             this.EmployeeNameTextBoxLabel3.TabIndex = 2;
             this.EmployeeNameTextBoxLabel3.TextChanged += new System.EventHandler(this.EmployeeNameTextBoxLabel3_TextChanged);
             // 
             // FullNameLabel3
             // 
             this.FullNameLabel3.AutoSize = true;
-            this.FullNameLabel3.Location = new System.Drawing.Point(41, 302);
+            this.FullNameLabel3.Location = new System.Drawing.Point(526, 259);
             this.FullNameLabel3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.FullNameLabel3.Name = "FullNameLabel3";
             this.FullNameLabel3.Size = new System.Drawing.Size(363, 37);
             this.FullNameLabel3.TabIndex = 3;
             this.FullNameLabel3.Text = "Please Enter Full Name:";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(1815, 311);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(634, 44);
-            this.dateTimePicker1.TabIndex = 4;
-            // 
-            // PleaseSelectTodayDateLabel4
-            // 
-            this.PleaseSelectTodayDateLabel4.AutoSize = true;
-            this.PleaseSelectTodayDateLabel4.Location = new System.Drawing.Point(1264, 311);
-            this.PleaseSelectTodayDateLabel4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.PleaseSelectTodayDateLabel4.Name = "PleaseSelectTodayDateLabel4";
-            this.PleaseSelectTodayDateLabel4.Size = new System.Drawing.Size(424, 37);
-            this.PleaseSelectTodayDateLabel4.TabIndex = 5;
-            this.PleaseSelectTodayDateLabel4.Text = "Please Select Today\'s Date: ";
             // 
             // BagListBox1
             // 
@@ -144,10 +125,10 @@
             this.PleaseSelectYourQuantityLabel6.TabIndex = 8;
             this.PleaseSelectYourQuantityLabel6.Text = "2. Please Select Your Quantity";
             // 
-            // comboBox1
+            // SelectQuantityComboBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.SelectQuantityComboBox1.FormattingEnabled = true;
+            this.SelectQuantityComboBox1.Items.AddRange(new object[] {
             "1",
             "2",
             "3",
@@ -158,18 +139,18 @@
             "8",
             "9",
             "10"});
-            this.comboBox1.Location = new System.Drawing.Point(881, 583);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(475, 45);
-            this.comboBox1.TabIndex = 9;
-            this.comboBox1.Text = "Select Quantity";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.SelectQuantityComboBox1.Location = new System.Drawing.Point(881, 583);
+            this.SelectQuantityComboBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.SelectQuantityComboBox1.Name = "SelectQuantityComboBox1";
+            this.SelectQuantityComboBox1.Size = new System.Drawing.Size(475, 45);
+            this.SelectQuantityComboBox1.TabIndex = 9;
+            this.SelectQuantityComboBox1.Text = "Select Quantity";
+            this.SelectQuantityComboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // ClearButton2
             // 
             this.ClearButton2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClearButton2.Location = new System.Drawing.Point(2028, 534);
+            this.ClearButton2.Location = new System.Drawing.Point(1467, 687);
             this.ClearButton2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.ClearButton2.Name = "ClearButton2";
             this.ClearButton2.Size = new System.Drawing.Size(388, 122);
@@ -181,7 +162,7 @@
             // OrderButton4
             // 
             this.OrderButton4.BackColor = System.Drawing.SystemColors.Highlight;
-            this.OrderButton4.Location = new System.Drawing.Point(1467, 534);
+            this.OrderButton4.Location = new System.Drawing.Point(1467, 468);
             this.OrderButton4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.OrderButton4.Name = "OrderButton4";
             this.OrderButton4.Size = new System.Drawing.Size(388, 122);
@@ -195,15 +176,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(2751, 1512);
+            this.ClientSize = new System.Drawing.Size(1941, 848);
             this.Controls.Add(this.OrderButton4);
             this.Controls.Add(this.ClearButton2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.SelectQuantityComboBox1);
             this.Controls.Add(this.PleaseSelectYourQuantityLabel6);
             this.Controls.Add(this.PleaseSelectYourBagLabel5);
             this.Controls.Add(this.BagListBox1);
-            this.Controls.Add(this.PleaseSelectTodayDateLabel4);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.FullNameLabel3);
             this.Controls.Add(this.EmployeeNameTextBoxLabel3);
             this.Controls.Add(this.DescriptionLabel2);
@@ -223,12 +202,10 @@
         private System.Windows.Forms.Label DescriptionLabel2;
         private System.Windows.Forms.TextBox EmployeeNameTextBoxLabel3;
         private System.Windows.Forms.Label FullNameLabel3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Label PleaseSelectTodayDateLabel4;
         private System.Windows.Forms.ListBox BagListBox1;
         private System.Windows.Forms.Label PleaseSelectYourBagLabel5;
         private System.Windows.Forms.Label PleaseSelectYourQuantityLabel6;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox SelectQuantityComboBox1;
         private System.Windows.Forms.Button ClearButton2;
         private System.Windows.Forms.Button OrderButton4;
     }
--- BIS345-Week2-Exercise6-BagOrder/BIS345-Week2-Exercise6-BagOrder/Form1.cs
+++ BIS345-Week2-Exercise6-BagOrder/BIS345-Week2-Exercise6-BagOrder/Form1.cs
@@ -7,6 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Name: Morrese Morrison
+//Program: Bag Ordering Form
+//Class: BIS 345 - Advanced Application Development
+//Professor: Dr. Nathan Kilgore
+//Completion Time: 11/10/2023 11:20AM
+//University: Peirce College Philadelphia
+//Description: This Program Allows A User To Enter Their Name, Select A Bag, Select A Quantity
+//Then Click The Order Button To Order Their Bag. If Statements Have Been Invoked To Prevent
+//A User From Not Selecting A Bag, Quanity Or From Entering Their Name When Placing Their Order
+
 
 namespace BIS345_Week2_Exercise6_BagOrder
 {
@@ -20,7 +30,6 @@ namespace BIS345_Week2_Exercise6_BagOrder
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
-           
 
         }
 
@@ -31,12 +40,45 @@ namespace BIS345_Week2_Exercise6_BagOrder
 
         private void OrderButton4_Click(object sender, EventArgs e)
         {
-            string UserSelectionBagQuantity = comboBox1.GetItemText(comboBox1.SelectedItem);
-            string FullName = EmployeeNameTextBoxLabel3.Text;
+
+            //**MAIN LOGIC\\
 
 
+            //(Code Block - 1) = The User Will Enter Their Full Name Into The Text Box, It Will Then Feed That To string Variable "FullName"
+             string FullName = EmployeeNameTextBoxLabel3.Text;
+
+
+            //(Code Block - 2) = The User Will Select The Bag That They Would Like, This Will Then Feed string Variable "UserSelectionBag"
             string UserSelectionBag = BagListBox1.GetItemText(BagListBox1.SelectedItem);
-            MessageBox.Show("Full Name: " + FullName + " \n" + "Bag Orderd: " + UserSelectionBag + " \n" + "Quantity: " + UserSelectionBagQuantity.ToString());
+
+            //(Code Block - 3) = The User Will Select The Quantity That They Would Like, This Will Then Feed string Variable "UserSelectionBagQuantity"
+            string UserSelectionBagQuantity = SelectQuantityComboBox1.GetItemText(SelectQuantityComboBox1.SelectedItem);
+
+            //(Code Block - 4) = If Statement That Determines If A User Enters Their Full Name, If They Select A Bag, Of If They Select A Quantity
+            //If Not, They Will Receive The Appropriate Error Messages Prompting Them To Do So. If They Have, The Message Box Will Print
+            if (FullName == String.Empty)
+            {
+                MessageBox.Show("Please Enter Your Full Name");
+            }
+            //Further Validation Can Be Invoked To Check For Numbers & Special Characters
+            if (BagListBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please Select A Bag");
+            }
+
+            if (SelectQuantityComboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Please Select A Quantity");
+            }
+  
+                 else
+                         {
+                            MessageBox.Show("ORDER CONFIRMATION" + " \n" + "Full Name: " + FullName + " \n" + "Bag Orderd: " 
+                             
+                             + UserSelectionBag + " \n" + "Quantity: " + UserSelectionBagQuantity.ToString());
+
+                                }
+            //CODE BLOCK - 4 **END\\
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -46,13 +88,18 @@ namespace BIS345_Week2_Exercise6_BagOrder
 
         private void EmployeeNameTextBoxLabel3_TextChanged(object sender, EventArgs e)
         {
-
+            //**ATTENTION** = Maximum Set Characters Is 25 Within Properties
         }
 
         private void ClearButton2_Click(object sender, EventArgs e)
         {
+            //**EXTRAS (1)** - All Three Of These Methods Will Clear Any Entries Within The Text Box & Any Selection In The ComboBox & ListBox\\
             BagListBox1.ClearSelected();
+            SelectQuantityComboBox1.SelectedItem = null;
+            EmployeeNameTextBoxLabel3.Clear();
+            //END CODE Block - EXTRAS (1)\\
             
         }
+
     }
 }
