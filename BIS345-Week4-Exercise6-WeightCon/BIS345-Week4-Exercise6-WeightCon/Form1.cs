5531a1302bc8f2e628b433308ac6fcaf8afe48dd
MorreseMorrison <rese298@gmail.com>
2023-11-24 19:01:57 -0500

    Add files via upload

--- BIS345-Week4-Exercise6-WeightCon/BIS345-Week4-Exercise6-WeightCon/Form1.cs
+++ BIS345-Week4-Exercise6-WeightCon/BIS345-Week4-Exercise6-WeightCon/Form1.cs
@@ -7,6 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Name: Morrese Morrison
+//Date Completed: 11/24/2023 6:51PM
+//University: Peirce College
+//Course: BIS 345 Advanced Application Development
+//Professor: Dr. Nathan Kilgore
+//Program Description: This Program Converts A Pound Value Into Kilograms, Ounces, And Grams. From The Please Enter TextBox
+//A Value Is Entered. The Value Can Not Be Negative Or Zero. There Is An If Statement That Is In Place To Handle That Non Valid Value Submission.
+//In Addition There Is A Try Catch Exception Handle When A User Attempts To Enter Anything Other Then A Number, As Well As Entering Nothing At All.
+//Once A Valid Value Is Submitted, That Value Is Then Fed Into Three Functions, To Generate The Final Conversion Value For Kilograms, Grams And Ounces.
+//This Program Also Consists Of 1 Abstract Parent Class, And Three Child Classess Derived From The Parent Class. Within The Parent Class, There Are
+//Three Methods That Complete The Pounds To Kilograms, Grams, and Ounces Conversion. All Three Methods Have A String Return Value That Is Then
+//Placed In The Appropriate Text Label.
 
 namespace BIS345_Week4_Exercise6_WeightCon
 {
@@ -153,54 +165,75 @@ namespace BIS345_Week4_Exercise6_WeightCon
 
             //**Main Logic**//
 
-            //(Code Block - 1 - START) This Code Takes The User's Input From The "Please Enter The Pound Weight Value"//
-            //This Value Is Parsed Into A Double Value And Assigned To The "UserPoundValueInput" variable//
-            double UserPoundValueInput = double.Parse(UserPoundValueEnterTextBox.Text);
-            //(Code Block - 1 - END)//
 
-           // try
-            //{
+            //Try Catch Exception Handling Method, Try Catch Is Looking For System.FormatException, When A Letter, Or Nothing
+            //Is Entered Into The UserPoundValueEnterTextBox, If The Exception Occurs, The Catch Portion Will Print An Error Message
+            try
+            {
 
-
-
-          //  }
-           // catch (Exception e)
-        //    {
-                // code that handles the exception
-        //    }
-
-            //(Code Block - 2 START)//
-            //(2.1 - Object Creation)//
-            PoundsToKiloGrams KiloGramObject = new PoundsToKiloGrams();
-
-            //(2.2 - Object "KiloGramObject" Calling The Pounds To KiloGramsMethod With Parameter. Returns To The KiloGramsTextBox)//
-            KiloGramsTextBox.Text = KiloGramObject.PoundsToKilogramsMethod(UserPoundValueInput);
-            //(Code Block - 2 - END)//
+                //(Code Block - 1 - START) This Code Takes The User's Input From The "Please Enter The Pound Weight Value"//
+                //This Value Is Parsed Into A Double Value And Assigned To The "UserPoundValueInput" variable//
+                double UserPoundValueInput = double.Parse(UserPoundValueEnterTextBox.Text);
+               
 
 
+                    //(Code Block 1.2) This If Statement Test Of The Entered Value Is A Negative Number Or Equal To Zero, If This Is
+                    //True, The Message Box Will Appear, And The Program Will Restart
+                    if (UserPoundValueInput <= 0)
+                    {
 
-            //(Code Block - 3 START)//
-            //(3.1 - Object Creation)//
-            PoundsToOunces OuncesObject = new PoundsToOunces();
+                        MessageBox.Show("You Can Not Enter A Zero Or A Negative Value, Please Try Again");
+                        Application.Restart();
 
-            //(3.1 - Object "OuncesObject" Calling The Pounds To Ounces Method With Parameter. Returns To The OuncesTextBox)//
-            OuncesTextBox.Text = OuncesObject.PoundsToOuncesMethod(UserPoundValueInput);
-            //(Code Block - 3 - END)//
+                    }
 
 
+                //(Code Block - 1 - END)//
 
-            //(Code Block - 4 START)//
-            //(4.1 - Object Creation)//
-            PoundsToGrams GramsObject = new PoundsToGrams();
 
-            //(4.2 - Object "GramsObject" Calling The PoundsToGramsMethod With Parameter. Returns To The GramsTextBox)//
-            GramsTextBox.Text = GramsObject.PoundsToGramsMethod(UserPoundValueInput);
-            //(Code Block - 4 - END)//
+                //(Code Block - 2 START)//
+                //(2.1 - Object Creation)//
+                PoundsToKiloGrams KiloGramObject = new PoundsToKiloGrams();
+
+                //(2.2 - Object "KiloGramObject" Calling The Pounds To KiloGramsMethod With Parameter. Returns To The KiloGramsTextBox)//
+                KiloGramsTextBox.Text = KiloGramObject.PoundsToKilogramsMethod(UserPoundValueInput);
+                //(Code Block - 2 - END)//
+
+
+
+                //(Code Block - 3 START)//
+                //(3.1 - Object Creation)//
+                PoundsToOunces OuncesObject = new PoundsToOunces();
+
+                //(3.1 - Object "OuncesObject" Calling The Pounds To Ounces Method With Parameter. Returns To The OuncesTextBox)//
+                OuncesTextBox.Text = OuncesObject.PoundsToOuncesMethod(UserPoundValueInput);
+                //(Code Block - 3 - END)//
+
+
+
+                //(Code Block - 4 START)//
+                //(4.1 - Object Creation)//
+                PoundsToGrams GramsObject = new PoundsToGrams();
+
+                //(4.2 - Object "GramsObject" Calling The PoundsToGramsMethod With Parameter. Returns To The GramsTextBox)//
+                GramsTextBox.Text = GramsObject.PoundsToGramsMethod(UserPoundValueInput);
+                //(Code Block - 4 - END)//
+
+            }
+
+
+            catch (System.FormatException)
+            {
+
+                MessageBox.Show("You Did Not Enter A Number Value, Please Try Again");
+
+            }
+
 
 
         }
 
-        private void ClearButton_Click(object sender, EventArgs e)
+         private void ClearButton_Click(object sender, EventArgs e)
         {
             //(Button - 2 Clear Button) = This Clears All Of The Text Boxes **//
             UserPoundValueEnterTextBox.Text = String.Empty;
