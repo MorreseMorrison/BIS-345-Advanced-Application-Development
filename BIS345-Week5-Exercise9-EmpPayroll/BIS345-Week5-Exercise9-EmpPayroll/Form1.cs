6a56364883b76f25fffa275b540236a70bde59a8
MorreseMorrison <rese298@gmail.com>
2023-12-06 23:05:26 -0500

    Add files via upload

--- BIS345-Week5-Exercise9-EmpPayroll/BIS345-Week5-Exercise9-EmpPayroll/Form1.cs
+++ BIS345-Week5-Exercise9-EmpPayroll/BIS345-Week5-Exercise9-EmpPayroll/Form1.cs
@@ -36,20 +36,19 @@ namespace BIS345_Week5_Exercise9_EmpPayroll
 
 
             //**PAYROLL CALCULATION SECTION START**\\
+
             try
             {
 
-                //(Code Block - 1) = User Enter Values Via Text Labels
+                //(CODE BLOCK - 1 - START) = User Enter Values Via Text Labels
                 EmployeeFullNameEnter = EnterFullNameTextBox.Text;
                 EmployeeIDEnter = EnterEmployeeIDTextBox.Text;
                 EmployeePayRateEnter = double.Parse(EnterPayrateTextbox.Text);
                 EmployeeHoursWorkedEnter = double.Parse(EnterHoursWorkedTextbox.Text);
 
 
-                //(Code Block - 2) = If Statements Then Execution Within Try Catch Block
-
+                //(Code Block - 2 - START) = If & Else Statements Then  Payroll Calculation
                 //(Code Block - 2.1)\\
-
                 //**IF STATEMENT**\\ If The Rate Entered Is Greater Than or Equal To 50, Print Error Message & Clear EnterPayRateTextBox
                 if (EmployeePayRateEnter >= 50)
                 {
@@ -58,9 +57,9 @@ namespace BIS345_Week5_Exercise9_EmpPayroll
                     EnterPayrateTextbox.Text = String.Empty;
 
                 }
+                //(Code Block - 2.1)\\
 
                 //(Code Block - 2.2)\\
-
                 //**IF STATEMENT**\\ If The Hours Worked Entered Is Greater Than or Equal To 60, Print Error Message & Clear EnterHoursWorkedTextbox
                 if (EmployeeHoursWorkedEnter >= 60)
                 {
@@ -69,29 +68,52 @@ namespace BIS345_Week5_Exercise9_EmpPayroll
                     EnterHoursWorkedTextbox.Text = String.Empty;
 
                 }
-
-
+                //(Code Block - 2.2)\\
 
                 //(Code Block - 2.3)\\
-
                 //**IF STATEMENT**\\ If The Hours Worked Entered Is Less Than or Equal To 40, Run Regular Total Pay Calculation Then Print Total To TotalPayTextBox
                 if (EmployeeHoursWorkedEnter <= 40)
 
                 {
-
+                    //PART 1//
+                    //**START Regualr Pay Calculation Logic**\\
                     double RegualrHoursTotalPay = EmployeeHoursWorkedEnter * EmployeePayRateEnter;
 
                     String DisplayRegualrTotalPay = Convert.ToString(RegualrHoursTotalPay);
 
-                    TotalPayTextBox.Text = String.Format("$" + DisplayRegualrTotalPay + " (Regular Pay Rate)");
-                    
+                    TotalPayTextBox.Text = String.Format("$" + DisplayRegualrTotalPay);
+
+                    PayTypeTextBox.Text = ("Regular Pay Rate");
+
+                    //**END Time & Half Calculation Logic**\\
+                    //PART 1//
+
+
+
+                    //PART 2//
+                    //**START Export File Logic** = Sends The Text File To The Users Public Documents Utilizes The File.WriteAllText Method**\\
+                    File.WriteAllText(@"C:\Users\Public\Documents\PeirceCollegePayrollExportTimeHalfPay.txt", "PEIRCE COLLEGE HOURLY PAYROLL EXPORT" + "\n" +
+                     "\n" + "Full Name: " + EmployeeFullNameEnter + "\n" + "Total Gross Pay: " + "$" + DisplayRegualrTotalPay + "\n" + "Employee ID: " +
+                      EmployeeIDEnter + "\n" + "Payrate: " + "$" + EmployeePayRateEnter + "\n" + "Hours Worked: " + EmployeeHoursWorkedEnter
+                      + "\n" + "Pay Type: Regular Pay Rate");
+
+                    //Message Box Telling The User To Check Their Public Documents Folder For The Export File
+
+                    MessageBox.Show("Please Check Your Public Documents Folder For Your Export" + "\n" + "Example: C:/Users/Public/Documents/PeirceCollegePayrollExport.txt");
+
+                    //**END Export File Logic**\\
+                    //PART 2//
+
                 }
+                //(Code Block - 2.3)\\
 
-                //(Code Block - 2.4)\\
-
-                //**ELSE**\\ Else Run Time And A Half Calculation Then Print To  TotalPayTextBox
+                //(Code Block - 2.4)\\      
+                //**ELSE**\\ Else Run Time And A Half Calculation Then Print To TotalPayTextBox
                 else
                 {
+
+                    //PART 1//
+                   //**START Time & Half Calculation Logic**\\
                     
                    double TimeAndAHalfTRate =  1.5 * EmployeePayRateEnter;
 
@@ -103,30 +125,36 @@ namespace BIS345_Week5_Exercise9_EmpPayroll
 
                    PayTypeTextBox.Text = ("Time & Half Pay Rate");
 
-
-                    // OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-                    // openFileDialog1.ShowDialog();
-
-                    
+                   //**END Time & Half Calculation Logic**\\
+                   //PART 1//
 
 
+                  
+   
+                   //PART 2//
+                   //**START Export File Logic** = Sends The Text File To The Users Public Documents Utilizes The File.WriteAllText Method**\\
 
-                    File.WriteAllText(@"C:\Users\Public\Documents\PeirceCollegePayrollExport.txt", EmployeeFullNameEnter + "$" + DisplayTimeAndAHalfTotalPay);
-                    string readText = File.ReadAllText(@"C:\Users\Public\Documents\PeirceCollegePayrollExport.txt");
-                    Console.WriteLine(readText);
+                    File.WriteAllText(@"C:\Users\Public\Documents\PeirceCollegePayrollExportTimeHalfPay.txt", "PEIRCE COLLEGE HOURLY PAYROLL EXPORT" + "\n" +
+                       "\n" + "Full Name: " + EmployeeFullNameEnter + "\n" + "Total Gross Pay: " + "$" + DisplayTimeAndAHalfTotalPay + "\n" + "Employee ID: " +
+                        EmployeeIDEnter + "\n" + "Payrate: " + "$" + EmployeePayRateEnter + "\n" + "Hours Worked: " + EmployeeHoursWorkedEnter 
+                        + "\n" + "Pay Type: Time & Half Pay Rate");
+
+                    //Message Box Telling The User To Check Their Public Documents Folder For The Export File
 
                     MessageBox.Show("Please Check Your Public Documents Folder For Your Export" + "\n" + "Example: C:/Users/Public/Documents/PeirceCollegePayrollExport.txt");
 
-
+                   //**END Export File Logic**\\
+                   //PART 2//
                 }
+                //(Code Block - 2.4)\\                
+                //(CODE BLOCK - 2 - END) 
 
             }
 
             catch (System.FormatException)
             {
 
-                MessageBox.Show("You Did Not Enter A Number Value, Please Try Again"); 
+                MessageBox.Show("Please Enter Only Letters For The Full Name Field & Numbers For The Payrate, Employee ID and Hours Worked Field"); 
 
             }
 
@@ -165,6 +193,15 @@ namespace BIS345_Week5_Exercise9_EmpPayroll
 
 
             openFileDialog1.InitialDirectory = @"C:\";
+
+            // OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // openFileDialog1.ShowDialog();
+
+            //
+            // string readText = File.ReadAllText(@"C:\Users\Public\Documents\PeirceCollegePayrollExportTimeHalfPay.txt");
+            // Console.WriteLine(readText);
+
 
         }
 
