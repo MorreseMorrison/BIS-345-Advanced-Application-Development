78bddfad0d374f30c26741d6dc6387e4bc299405
MorreseMorrison <rese298@gmail.com>
2023-12-12 19:12:23 -0500

    Delete BIS345-Week6-Exercise7-BooksDB directory

--- BIS345-Week6-Exercise7-BooksDB/Program Documentation/Database Development Documentation/SQL Server Queries/BIS345 - Query 3 (Inner Join Query College Course Books).sql
+++
@@ -1,4 +0,0 @@
-SELECT  CourseBookTable.Course_Number, PeirceCollegeCourseCatalogTable.Course_Name, BookTable.Title, BookTable.ISBN 
-FROM ((BookTable
-INNER JOIN CourseBookTable ON BookTable.ISBN = CourseBookTable.ISBN)
-INNER JOIN PeirceCollegeCourseCatalogTable ON CourseBookTable.Course_Number = PeirceCollegeCourseCatalogTable.Course_Number);
